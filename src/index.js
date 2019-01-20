26a8b1cafad6b0cfe3e677e11fc39cb36e1bbbf8
Brian Vanderbusch <brian.vanderbusch@gmail.com>
2019-01-20 11:34:15 -0600

    complete 'A Simple Query'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 const { GraphQLServer } = require('graphql-yoga')
 
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
 // 1
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
 }
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+  }
 `
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   }
 }
 
