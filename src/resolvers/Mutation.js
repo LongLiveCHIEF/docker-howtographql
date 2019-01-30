ac10b9520c3dcb97edc367c012e81564bb836d11
Brian Vanderbusch <brian.vanderbusch@gmail.com>
2019-01-30 16:20:42 -0600

    Implementing the Authentication flow

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { APP_SECRET, getUserId } = require('../utils')
+
 async function signup(parent, args, context, info) {
   // 1
   const password = await bcrypt.hash(args.password, 10)
@@ -12,6 +16,15 @@ async function signup(parent, args, context, info) {
     token,
     user,
   }
+}
+
+function post(parent, args, context, info) {
+  const userId = getUserId(context)
+  return context.prisma.createLink({
+    url: args.url,
+    description: args.description,
+    postedBy: { connect: { id: userId } },
+  })
 }
 
 async function login(parent, args, context, info) {
